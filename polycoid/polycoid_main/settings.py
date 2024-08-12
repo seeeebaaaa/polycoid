a78ab41a117e49b8e55de017a37e1c9d86fb6bd9
seeeebaaaa <>
2024-08-12 19:29:49 +0200

    added app filmliste

--- polycoid/polycoid_main/settings.py
+++ polycoid/polycoid_main/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'polycoid',
-        'USER': 'uwu',
+        'USER': '',
         'PASSWORD': '',
         #'HOST': 'localhost', # in prod
         #'PORT': '', # in prod
