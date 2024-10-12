2351e56ace86083b7d9b10b0c9570ceeade33b43
seeeebaaaa <>
2024-10-12 19:11:24 +0200

    Added deploy

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Activate virtual environment
+source /var/www/polycoid/.venv/bin/activate
+
+# Navigate to the project directory
+cd /var/www/polycoid
+
+# Pull the latest changes from GitHub
+git pull origin main
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Restart Gunicorn
+sudo systemctl restart gunicorn
+
+# Restart Nginx (optional)
+sudo systemctl restart nginx
